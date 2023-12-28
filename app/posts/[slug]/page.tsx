1967fe6ed03c948ef0cb8053b4be06f7d645dda0
Pau Garcia <paugarcia32@gmail.com>
2023-12-28 20:03:08 +0100

    first commit3

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,10 @@
 import fs from "fs";
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
-import getPostMetadata from "../../../components/getPostMetadata";
+import getPostMetadata from "@/components/getPostMetadata";
+import TOC from "@/components/TOC"
+import { SearchIcon, TagIcon } from '@/components/icons';
+import { Chip, Image } from '@nextui-org/react';
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
@@ -22,15 +25,29 @@ const PostPage = (props: any) => {
   const slug = props.params.slug;
   const post = getPostContent(slug);
   return (
-    <div>
+    <div className="">
       <div className="my-12 text-center">
-        <h1 className="text-2xl text-slate-600 ">{post.data.title}</h1>
+        <h1 className="text-2xl font-title ">{post.data.title}</h1>
         <p className="text-slate-400 mt-2">{post.data.date}</p>
       </div>
 
-      <article className="prose">
-        <Markdown>{post.content}</Markdown>
-      </article>
+      <div className="max-w-4xl m-0 mx-auto  leading-relaxed px-25 md:px-40">
+        <div className="flex justify-start items-center gap-2 flex-wrap mb-4">
+          <TagIcon />
+          {post.data.tags && post.data.tags.length > 0 && post.data.tags.map((tag: string, index: number) => (
+            <Chip key={index} className="bg-primary" size='sm'>
+              <h1 className="font-semibold">{tag}</h1>
+            </Chip>
+          ))}
+        </div>
+        <TOC />
+        <article
+        className="prose dark:prose-invert prose:font-body prose-headings:font-title prose-headings:font-bold prose-h2:text-2xl prose-h2:mt-7.5 prose-h2:mb-2.5 prose-h3:text-xl prose-h3:mt-6 prose-h3:mb-2 prose-h4:text-lg prose-h4:mt-5 prose-h4:mb-1.5 prose-a:text-accent hover:prose-a:underline prose-a:no-underline "
+
+        >
+          <Markdown>{post.content}</Markdown>
+        </article>
+      </div>
     </div>
   );
 };
